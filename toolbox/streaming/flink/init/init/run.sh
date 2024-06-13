93e00035d311b4eeea85138c331163b491064743
BIZIMUNGU Steve <steve.bizimungu-prestataire@docaposte.fr>
2024-06-13 19:42:40 +0200

    initialise

---
+++ toolbox/streaming/flink/init/init/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+docker cp target/init-0.0.1-SNAPSHOT.jar init-jobmanger-1:/lib/flink/
---
+++ toolbox/streaming/flink/init/init/src/main/java/com/bees/streaming/processor/init/InitApplication.java
@@ -0,0 +1,20 @@
+package com.bees.streaming.processor.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitApplication {
+
+	public static void main(String[] args) {
+
+        try {
+            FraudDetectionJob.run();
+			SpringApplication.run(InitApplication.class, args);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+	}
+
+}
